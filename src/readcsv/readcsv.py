e51676ddf26bafc3cd6dc5a656dfc18f6005b8b2
Srikanth Gadadasu <sriyan@Srikanths-MBP.T-mobile.com>
2020-06-11 09:31:32 -0400

    read csv

--- src/readcsv/readcsv.py
+++ src/readcsv/readcsv.py
@@ -1,49 +1,50 @@
+# Copyright 2020 The Nadi Data Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from concurrent.futures import ThreadPoolExecutor
+
 class Readcsv():
     
-    def __init__(self, file, read_mode=None, chunk_size=None, row_sep=None):
+    def __init__(self, file, read_mode=None, chunk_size=None, row_sep=None, delim=None):
         self.file = file
         self.read_mode = read_mode
         self.chunk_size = chunk_size
         self.row_sep = row_sep
+        self.delim = delim
 
     def read_in_chunks(self, f):
         """Read file in chunks for processing"""
-        curr_row = ''
+        curr_rec = ''
         while True:
             chunk = f.read(self.chunk_size)
             if chunk == '': # End of the file
-                yield curr_row
+                yield curr_rec
                 break
             while True:
                 i = chunk.find(self.row_sep)
                 if i == -1:
                     break
-                yield curr_row + chunk[:i]
-                curr_row = ''
+                yield curr_rec + chunk[:i]
+                curr_rec = ''
                 chunk = chunk[i+1:]
-            curr_row += chunk
+            curr_rec += chunk
 
 
-    def process_chunks(self):
+    def process_chunks_in_parallel(self):
         with open(self.file, self.read_mode) as f:
-            lines = []
             row_count = 0
-            """ Process each data chunk retrieved from file"""
-            for piece in self.read_in_chunks(f):
-                if piece == '':
+            for line in self.read_in_chunks(f):
+                if line == '':
                     break
-                line = piece.split(',')
+                record = line.strip().split(self.delim) # Convert the record into array using delimiter
                 """ Convert Header into columns"""
                 if row_count == 0:
-                    header = line
+                    header = record
                     row_count += 1
                     continue
                 else:
-                    """ Prepare column , value pairs in form of dictionary"""
-                    line = dict(zip(header,line))
-                    lines.append(line)
-                    yield lines
-                    lines.pop()
+                    """ Prepare column , value pairs in the form of dictionary"""
+                    record = dict(zip(header,record))
+                    yield record
                     row_count += 1
-
-
--- src/readcsv/test_readcsv.py
+++ src/readcsv/test_readcsv.py
@@ -1,6 +1,13 @@
+# Copyright 2020 The Nadi Data Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import readcsv
+import timeit
 
-obj1 = readcsv.Readcsv('/Users/sriyan/Downloads/sales.csv','r',1024,'\n')
+t1 = timeit.default_timer()
+csv_data_generator = readcsv.Readcsv('/Users/sriyan/Downloads/sales_50mb_1500000.csv','r',5000,'\n',',').process_chunks_in_parallel()
+print(len(list(csv_data_generator)))
+print("{} Seconds Needed for ProcessPoolExecutor".format(timeit.default_timer() - t1))
 
-for record in obj1.process_chunks():
-    print(record[0]['Country'])
+# 11.09 seconds for print(len(list(csv_data_generator)))
