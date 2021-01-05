e1dde307b5a91fb4bd08402a9be512a1e9430a89
Sri <sreenews123@gmail.com>
2021-01-04 22:09:32 -0500

    read json

--- src/sample_beam.py
+++ src/sample_beam.py
@@ -1,16 +1,15 @@
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import PipelineOptions, DirectOptions
 from csv import reader
 from collections import namedtuple
 import typing
 import logging
+import argparse
 import timeit
-import dask.dataframe as dd
 import readtextfile
 import writetextfile
 
-pipeline_options = PipelineOptions(['--direct_num_workers', '6'], ['--direct_running_mode', 'multi_threading'])
-csv_file_current= '/Users/sripri/Downloads/1500000_Sales_Records.csv'
+csv_file_current= '/Users/sriyan/Downloads/5m_Sales_Records.csv'
 
 class IpSchema(typing.NamedTuple):
     Region: str
@@ -44,26 +43,31 @@ class UsCovidDataCsvReader(beam.DoFn):
     values = [int(val) if val.isdigit() else val for val in next(reader([element]))]
     return [self._schema(*values)]
 
-def read_write_csv(IpSchema, pipeline_options):
-    with beam.Pipeline(options=pipeline_options) as p:
+def read_write_csv(IpSchema, csv_file_current):
+    options = PipelineOptions([
+    "--runner=FlinkRunner",
+    "--flink_master=localhost:8081",
+    "--environment_type=LOOPBACK"])
+    with beam.Pipeline(options=options) as p:
         csv_data = p | 'Create PCollection from files' >> beam.io.ReadFromText(csv_file_current, skip_header_lines=1)
         current_data = csv_data | 'Parse' >> beam.ParDo(UsCovidDataCsvReader(UsCovidData))
-        (current_data | 'Filter data' >> beam.Filter(lambda row : row.Region == 'Europe')
-                      | 'NamedTuple to tuple' >> beam.Map(lambda row : tuple(row))
-                      | 'Write text file' >> beam.io.WriteToText('/Users/sripri/Downloads/test', file_name_suffix='.txt').with_output_types(IpSchema))
+        (current_data | 'Filter data' >> beam.Filter(lambda row : row.Region== 'Europe')
+                      #| 'NamedTuple to tuple' >> beam.Map(lambda row : tuple(row))
+                      | 'Write text file' >> beam.io.WriteToText('/Users/sriyan/Downloads/test', file_name_suffix='.txt'))
 
 if __name__ == '__main__':
     t1 = timeit.default_timer()
-    #logging.getLogger().setLevel(logging.INFO)
-    read_write_csv(IpSchema, pipeline_options)
+    logging.getLogger().setLevel(logging.INFO)
+    read_write_csv(IpSchema, csv_file_current)
     print(timeit.default_timer() - t1)
-
+    
+    """
     t2 = timeit.default_timer()
     df = readtextfile.ReadTextFile(ipfile=csv_file_current,
                                    ipschemafile='schema/sample_csv_file.txt',
-                                   delimiter=',', skiprows=1, parallel=6).read_using_dask()
+                                   delimiter=',', skiprows=1, parallel=4).read_using_dask()
     df = df[df['Region'] == 'Europe']
-    writetextfile.WriteTextFile(ipdf=df, filename="/Users/sripri/Downloads/test.txt",
+    writetextfile.WriteTextFile(ipdf=df, filename="/Users/sriyan/Downloads/test.txt",
                                 single_file=False, encoding='utf-8', sep=',', header=False).write_using_dask()
-    print(timeit.default_timer() - t2)
+    print(timeit.default_timer() - t2) """
 
